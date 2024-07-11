b795d2994564f7091e1f102ae0392f7e1d7a0556
nelanjoe <nelanjoe@gmail.com>
2024-07-11 01:18:46 +0000

    refactor component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,34 +9,34 @@ const { increment, decrement, reset } = store;
 </script>
 
 <template>
-  <section>
-    <div>
-      <p>Ini adalah aplikasi yang dibangun dengan Vue + Pinia</p>
-    </div>
+  <section class="grid place-content-center h-screen">
+    <div
+      class="flex flex-col items-center p-4 rounded-lg border shadow-md jutify-center w-fit"
+    >
+      <div class="my-6">
+        <p class="text-2xl font-semibold">{{ count }}</p>
+      </div>
 
-    <div class="my-6">
-      <p class="text-2xl font-semibold">{{ count }}</p>
-    </div>
-
-    <div class="flex flex-row gap-3">
-      <button
-        class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md"
-        @click="increment"
-      >
-        Increment
-      </button>
-      <button
-        class="px-4 py-2 font-semibold text-white bg-rose-500 rounded-md"
-        @click="reset"
-      >
-        Reset
-      </button>
-      <button
-        class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md"
-        @click="decrement"
-      >
-        Decrement
-      </button>
+      <div class="flex flex-row gap-3">
+        <button
+          class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md"
+          @click="increment"
+        >
+          Increment
+        </button>
+        <button
+          class="px-4 py-2 font-semibold text-white bg-rose-500 rounded-md"
+          @click="reset"
+        >
+          Reset
+        </button>
+        <button
+          class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md"
+          @click="decrement"
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   </section>
 </template>
